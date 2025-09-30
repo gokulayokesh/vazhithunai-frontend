d7362b3618c95d85228dac46586dd3a58b954f85
gokulayokesh <gokulayokesh@gmail.com>
2025-09-30 18:12:33 +0530

    migration changes

--- database/migrations/2025_09_29_204241_add_mobile_column_in_users_table.php
+++ database/migrations/2025_09_29_204241_add_mobile_column_in_users_table.php
@@ -11,9 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('mobile')->unique()->default(null)->nullable()->after('email');
-        });
+        if (!Schema::hasColumn('users', 'mobile')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('mobile')
+                      ->unique()
+                      ->nullable()
+                      ->after('email');
+            });
+        }
     }
 
     /**
@@ -21,8 +26,11 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['mobile']);
-        });
+        if (Schema::hasColumn('users', 'mobile')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropUnique(['mobile']);
+                $table->dropColumn('mobile');
+            });
+        }
     }
 };
