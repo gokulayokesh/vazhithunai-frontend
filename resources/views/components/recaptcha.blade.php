b5aeb39222352a197f588214816c9b3455a21426
gokulayokesh <gokulayokesh@gmail.com>
2025-10-01 09:03:52 +0530

    recaptcha changes

--- resources/views/components/recaptcha.blade.php
+++ resources/views/components/recaptcha.blade.php
@@ -1,1 +1,1 @@
-<div class="g-recaptcha" data-sitekey="{{ config('services.recaptcha.site_key') }}"></div>
+<div class="g-recaptcha" data-sitekey="{{ env('RECAPTCHA_SITE_KEY', '6LcvE9orAAAAAD6V4lQVZk4iJxAtyVj9bl_RUdSa') }}"></div>
