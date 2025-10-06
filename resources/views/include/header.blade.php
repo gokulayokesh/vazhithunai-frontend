60c375af6a5a7f81c78ae52c19615d8843f7e1ce
gokulayokesh <gokulayokesh@gmail.com>
2025-10-06 22:20:33 +0530

    ga4 user id updates

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -17,7 +17,7 @@
     @auth
         <script>
             gtag('config', 'G-FK1KFKNP7E', {
-                user_id: '{{ auth()->user()->id ?? '' }}'
+                user_id: {{ auth()->user()->id ?? '' }}
             });
         </script>
     @else
